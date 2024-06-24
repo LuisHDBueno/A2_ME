66f0960a22853c1835e582e375b35703fc3c8de0
Luís Henrique Domingues Bueno <105614892+LuisHDBueno@users.noreply.github.com>
2024-06-23 23:24:34 -0300

    Add models

--- src/analise_exploratoria/analise_inicial.r
+++ src/analise_exploratoria/analise_inicial.r
@@ -1,46 +1,70 @@
+library(lme4)
 data <- read.csv("data/personagens_mod.csv", header = TRUE)
 summary(data)
+
+#Remover colunas que não serão utilizadas
+data$Distancia_da_Primeira_Aparicao_em_capitulos <- NULL
+
 
 set.seed(123)  # Define uma semente aleatória para reprodução
 index_train <- sample(1:nrow(data), 4000)
 data_train <- data[index_train, ]
 data_test <- data[-index_train, ]
 
-data$Numero_de_capitulos_ate_o_volume <- NULL
-data$Distancia_da_Primeira_Aparicao_em_capitulo <- NULL
+model1 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
+              + Grupo + Primeiro_Capitulo 
+              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga,
+              data = data_train, family = binomial)
 
-modelIntercept <- glm(Esta_na_Capa ~ 1, data = data_train, family = binomial)
-summary(modelIntercept)
-# Matriz de confusão treino
-pred <- predict(modelIntercept, newdata = data_test, type = "response")
-pred <- ifelse(pred > 0.5, 1, 0)
-table(data_test$Esta_na_Capa, pred)
-
-model1 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume, data = data_train, family = binomial)
 summary(model1)
-# Matriz de confusão treino
-pred <- predict(model1, newdata = data_test, type = "response")
-pred <- ifelse(pred > 0.5, 1, 0)
-table(data_test$Esta_na_Capa, pred)
 
-
-model2 <- glm(Esta_na_Capa ~ Grupo + Porcentagem_de_Capitulos_do_Volume + Primeiro_Capitulo + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga, data = data_train, family = binomial)
+model2 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
+              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga,
+              data = data_train, family = binomial)
 summary(model2)
 
-# Matriz de confusão
-pred <- predict(model2, newdata = data_test, type = "response")
-pred <- ifelse(pred > 0.5, 1, 0)
-# Tamanho da resposta
-length(pred)
-# Tamanho da resposta esperada
-length(data_test$Esta_na_Capa)
-table(data_test$Esta_na_Capa, pred)
-
-
-model3 <- glm(Esta_na_Capa ~ Grupo + Primeiro_Capitulo + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga, data = data_train, family = binomial)
+model3 <- glmer(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
+              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga
+              + (1|Protagonista), data = data_train, family = binomial)
 summary(model3)
 
-# Matriz de confusão
+model4 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
+              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga
+              + Protagonista + Protagonista * Porcentagem_de_Capitulos_do_Volume,
+              data = data_train, family = binomial)
+summary(model4)
+
+#Matriz de confusão modelo 1
+pred <- predict(model1, newdata = data_test, type = "response")
+pred <- ifelse(pred > 0.5, 1, 0)
+table <- table(data_test$Esta_na_Capa, pred)
+table
+
+#Matriz de confusão modelo 2
+pred <- predict(model2, newdata = data_test, type = "response")
+pred <- ifelse(pred > 0.5, 1, 0)
+table(data_test$Esta_na_Capa, pred)
+
+#Matriz de confusão modelo 3
 pred <- predict(model3, newdata = data_test, type = "response")
 pred <- ifelse(pred > 0.5, 1, 0)
 table(data_test$Esta_na_Capa, pred)
+
+#Matriz de confusão modelo 4
+pred <- predict(model4, newdata = data_test, type = "response")
+pred <- ifelse(pred > 0.5, 1, 0)
+table(data_test$Esta_na_Capa, pred)
+
+#Curva ROC
+install.packages("pROC")
+library(pROC)
+roc1 <- roc(data_test$Esta_na_Capa, predict(model1, newdata = data_test, type = "response"))
+roc2 <- roc(data_test$Esta_na_Capa, predict(model2, newdata = data_test, type = "response"))
+roc3 <- roc(data_test$Esta_na_Capa, predict(model3, newdata = data_test, type = "response"))
+roc4 <- roc(data_test$Esta_na_Capa, predict(model4, newdata = data_test, type = "response"))
+
+plot(roc1, col = "red")
+plot(roc2, col = "blue", add = TRUE)
+plot(roc3, col = "green", add = TRUE)
+plot(roc4, col = "yellow", add = TRUE)
+

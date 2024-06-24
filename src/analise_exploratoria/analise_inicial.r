e03bc266c7f07d92b87249fc53ab379227619a0f
Luís Henrique Domingues Bueno <105614892+LuisHDBueno@users.noreply.github.com>
2024-06-24 13:02:24 -0300

    Antes dos pesos

--- src/analise_exploratoria/analise_inicial.r
+++ src/analise_exploratoria/analise_inicial.r
@@ -5,11 +5,11 @@ summary(data)
 #Remover colunas que não serão utilizadas
 data$Distancia_da_Primeira_Aparicao_em_capitulos <- NULL
 
-
 set.seed(123)  # Define uma semente aleatória para reprodução
 index_train <- sample(1:nrow(data), 4000)
 data_train <- data[index_train, ]
 data_test <- data[-index_train, ]
+
 
 model1 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
               + Grupo + Primeiro_Capitulo 
@@ -24,15 +24,30 @@ model2 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume
 summary(model2)
 
 model3 <- glmer(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
-              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga
-              + (1|Protagonista), data = data_train, family = binomial)
-summary(model3)
+              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga + 
+              (1|Protagonista), data = data_train, family = binomial)
 
-model4 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
-              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga
-              + Protagonista + Protagonista * Porcentagem_de_Capitulos_do_Volume,
-              data = data_train, family = binomial)
+model4 <- glmer(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
+              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga + 
+              (1|Grupo), data = data_train, family = binomial)
 summary(model4)
+
+model5 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
+              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga + 
+                Distancia_da_Primeira_Aparicao_em_volumes, data = data_train, family = binomial)
+summary(model5)
+
+model6 <- glm(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
+              + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga + 
+                Distancia_da_Primeira_Aparicao_em_volumes + 
+                Numero_de_capitulos_ate_o_volume, data = data_train, family = binomial)
+summary(model6)
+
+model7 <- glmer(Esta_na_Capa ~ Porcentagem_de_Capitulos_do_Volume 
+                + Ultimo_Capitulo + E_a_Primeira_Aparicao_no_manga + 
+                  Distancia_da_Primeira_Aparicao_em_volumes + 
+                (1|Grupo), data = data_train, family = binomial)
+summary(model7)
 
 #Matriz de confusão modelo 1
 pred <- predict(model1, newdata = data_test, type = "response")
@@ -55,16 +70,23 @@ pred <- predict(model4, newdata = data_test, type = "response")
 pred <- ifelse(pred > 0.5, 1, 0)
 table(data_test$Esta_na_Capa, pred)
 
-#Curva ROC
-install.packages("pROC")
-library(pROC)
-roc1 <- roc(data_test$Esta_na_Capa, predict(model1, newdata = data_test, type = "response"))
-roc2 <- roc(data_test$Esta_na_Capa, predict(model2, newdata = data_test, type = "response"))
-roc3 <- roc(data_test$Esta_na_Capa, predict(model3, newdata = data_test, type = "response"))
-roc4 <- roc(data_test$Esta_na_Capa, predict(model4, newdata = data_test, type = "response"))
+#Matriz de confusão modelo 5
+pred <- predict(model5, newdata = data_test, type = "response")
+pred <- ifelse(pred > 0.5, 1, 0)
+table(data_test$Esta_na_Capa, pred)
 
-plot(roc1, col = "red")
-plot(roc2, col = "blue", add = TRUE)
-plot(roc3, col = "green", add = TRUE)
-plot(roc4, col = "yellow", add = TRUE)
+#Matriz de confusão modelo 6
+pred <- predict(model6, newdata = data_test, type = "response")
+pred <- ifelse(pred > 0.5, 1, 0)
+table(data_test$Esta_na_Capa, pred)
 
+#Matriz de confusão modelo 7
+pred <- predict(model7, newdata = data_test, type = "response")
+pred <- ifelse(pred > 0.5, 1, 0)
+table(data_test$Esta_na_Capa, pred)
+
+
+# True positive rate
+tpr <- function(table) {
+  table[2, 2] / sum(table[2, ])
+}
